097bab92324b3ba3acb5c887fd2d1b8f109d4ccd
Azeem Rasydan <azeem.rasydan@plus.com.my>
2024-04-09 09:54:39 +0800

    Restructured folder

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { RegistrationPage } from 'pages/registration/registration.component';
+import { RegistrationPage } from 'app/pages/registration/registration.component';
 
 export const routes: Routes = [
     {
